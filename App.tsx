4310662b3492e8d53c9c08f2c011f42886b918e7
wusilan <wusilan@qiyi.com>
2021-09-10 18:04:31 +0800

    comp: SectionList

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@
  */
 
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, FlatList} from 'react-native';
+import {StyleSheet, Text, View, SectionList} from 'react-native';
 import {
   Colors,
   DebugInstructions,
@@ -19,49 +19,44 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
-  const data = [
-    {id: 1, text: '通协小区'},
-    {id: 2, text: '广友小区'},
-    {id: 3, text: '红梅小区'},
-    {id: 31, text: '通协小区'},
-    {id: 40, text: '广友小区'},
-    {id: 33, text: '红梅小区'},
-    {id: 12, text: '通协小区'},
-    {id: 32, text: '广友小区'},
-    {id: 53, text: '红梅小区'},
-    {id: 16, text: '通协小区'},
-    {id: 72, text: '广友小区'},
-    {id: 83, text: '红梅小区'},
-    {id: 21, text: '通协小区'},
-    {id: 42, text: '广友小区'},
-    {id: 56, text: '红梅小区'},
-    {id: 71, text: '通协小区'},
-    {id: 82, text: '广友小区'},
-    {id: 35, text: '红梅小区'},
-    {id: 13, text: '通协小区'},
-    {id: 22, text: '广友小区'},
-    {id: 36, text: '红梅小区'},
-    {id: 19, text: '通协小区'},
-    {id: 23, text: '广友小区'},
-    {id: 30, text: '红梅小区'},
+  const mock = [
+    {title: '淞虹路', data: ['通协小区', '红梅小区']},
+    {title: '北新泾', data: ['广友小区', '虹康三期']},
+    {title: '北新泾', data: ['广友小区', '虹康三期']},
+    {title: '北新泾', data: ['广友小区', '虹康三期']},
+    {title: '北新泾', data: ['广友小区', '虹康三期']},
+    {title: '北新泾', data: ['广友小区', '虹康三期']},
   ];
   type Item = {
-    id: number;
-    text: string;
+    title: number;
+    data: string;
   };
-  const render = (record: {item: Item}) => <Text>{record.item.text}</Text>;
+  const render = (record: {item: Item}) => <Text>{record.item.data}</Text>;
   const [refreshing, setRefreshing] = useState(false);
   return (
-    <View style={{marginTop: 100}}>
-      <Text style={{fontSize: 20}}>hello world</Text>
-      <FlatList
-        data={data}
-        renderItem={render}
-        refreshing={refreshing}
-        keyExtractor={item => String(item.id)}
-        onRefresh={() => {
-          setRefreshing(false);
-        }}
+    <View style={{margin: 100}}>
+      <SectionList
+        sections={mock}
+        renderItem={({item}) => <Text>{item}</Text>}
+        renderSectionHeader={({section: {title}}) => (
+          <Text
+            style={{
+              fontSize: 20,
+              marginVertical: 10,
+              fontWeight: 'bold',
+              paddingVertical: 10,
+            }}>
+            {title}
+          </Text>
+        )}
+        ItemSeparatorComponent={() => (
+          <View
+            style={{
+              borderTopColor: 'rbga(0,0,0,0.03)',
+              borderWidth: 0.5,
+            }}
+          />
+        )}
       />
     </View>
   );
